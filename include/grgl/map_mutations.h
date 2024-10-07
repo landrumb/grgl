0c4117aa4c7fbc17049ed8295eabe008ce5a1ff0
Ben Landrum <8enlandrum@gmail.com>
2024-10-07 12:42:56 -0700

    atomic stats

--- include/grgl/map_mutations.h
+++ include/grgl/map_mutations.h
@@ -20,6 +20,8 @@
 #include "grgl/common.h"
 #include "grgl/mut_iterator.h"
 
+#include <atomic>
+#include <memory>
 #include <iosfwd>
 
 namespace grgl {
@@ -27,42 +29,89 @@ namespace grgl {
 class MutableGRG;
 using MutableGRGPtr = std::shared_ptr<MutableGRG>;
 
+
 struct MutationMappingStats {
-    size_t totalMutations{};
-    size_t emptyMutations{};
-    size_t mutationsWithOneSample{};
-    size_t mutationsWithNoCandidates{};
-    size_t reusedNodes{};
-    size_t reusedNodeCoverage{};
-    size_t reusedExactly{};
-    size_t singletonSampleEdges{}; // Direct edge from Mutation to sample
-    size_t newTreeNodes{};
-    size_t samplesProcessed{};
-    size_t numCandidates{};
-    size_t reuseSizeBiggerThanHistMax{};
-    size_t numWithSingletons{};
-    size_t maxSingletons{};
-    size_t reusedMutNodes{};
+    std::atomic<size_t> totalMutations{0};
+    std::atomic<size_t> emptyMutations{0};
+    std::atomic<size_t> mutationsWithOneSample{0};
+    std::atomic<size_t> mutationsWithNoCandidates{0};
+    std::atomic<size_t> reusedNodes{0};
+    std::atomic<size_t> reusedNodeCoverage{0};
+    std::atomic<size_t> reusedExactly{0};
+    std::atomic<size_t> singletonSampleEdges{0}; // Direct edge from Mutation to sample
+    std::atomic<size_t> newTreeNodes{0};
+    std::atomic<size_t> samplesProcessed{0};
+    std::atomic<size_t> numCandidates{0};
+    std::atomic<size_t> reuseSizeBiggerThanHistMax{0};
+    std::atomic<size_t> numWithSingletons{0};
+    std::atomic<size_t> maxSingletons{0};
+    std::atomic<size_t> reusedMutNodes{0};
     std::vector<size_t> reuseSizeHist;
 
+    MutationMappingStats() = default;
+
+    // copy constructor
+    MutationMappingStats(const MutationMappingStats& other) : 
+        totalMutations(other.totalMutations.load()), 
+        emptyMutations(other.emptyMutations.load()), 
+        mutationsWithOneSample(other.mutationsWithOneSample.load()),
+        mutationsWithNoCandidates(other.mutationsWithNoCandidates.load()),
+        reusedNodes(other.reusedNodes.load()),
+        reusedNodeCoverage(other.reusedNodeCoverage.load()),
+        reusedExactly(other.reusedExactly.load()),
+        singletonSampleEdges(other.singletonSampleEdges.load()),
+        newTreeNodes(other.newTreeNodes.load()),
+        samplesProcessed(other.samplesProcessed.load()),
+        numCandidates(other.numCandidates.load()),
+        reuseSizeBiggerThanHistMax(other.reuseSizeBiggerThanHistMax.load()),
+        numWithSingletons(other.numWithSingletons.load()),
+        maxSingletons(other.maxSingletons.load()),
+        reusedMutNodes(other.reusedMutNodes.load()),
+        reuseSizeHist(other.reuseSizeHist) {}
+
+    // copy assignment
+    MutationMappingStats& operator=(const MutationMappingStats& other) {
+        if (this == &other) return *this; // handle self-assignment
+
+        totalMutations.store(other.totalMutations.load());
+        emptyMutations.store(other.emptyMutations.load());
+        mutationsWithOneSample.store(other.mutationsWithOneSample.load());
+        mutationsWithNoCandidates.store(other.mutationsWithNoCandidates.load());
+        reusedNodes.store(other.reusedNodes.load());
+        reusedNodeCoverage.store(other.reusedNodeCoverage.load());
+        reusedExactly.store(other.reusedExactly.load());
+        singletonSampleEdges.store(other.singletonSampleEdges.load());
+        newTreeNodes.store(other.newTreeNodes.load());
+        samplesProcessed.store(other.samplesProcessed.load());
+        numCandidates.store(other.numCandidates.load());
+        reuseSizeBiggerThanHistMax.store(other.reuseSizeBiggerThanHistMax.load());
+        numWithSingletons.store(other.numWithSingletons.load());
+        maxSingletons.store(other.maxSingletons.load());
+        reusedMutNodes.store(other.reusedMutNodes.load());
+        reuseSizeHist = other.reuseSizeHist;
+
+        return *this;
+    }
+
     void print(std::ostream& outStream) const {
-        outStream << "mutations: " << this->totalMutations << std::endl;
-        outStream << "candidates: " << this->numCandidates << std::endl;
-        outStream << "emptyMutations: " << this->emptyMutations << std::endl;
-        outStream << "mutationsWithOneSample: " << this->mutationsWithOneSample << std::endl;
-        outStream << "singletonSampleEdges: " << this->singletonSampleEdges << std::endl;
-        outStream << "samplesProcessed: " << this->samplesProcessed << std::endl;
-        outStream << "reusedNodes: " << this->reusedNodes << std::endl;
-        outStream << "reusedExactly: " << this->reusedExactly << std::endl;
-        outStream << "reusedNodeCoverage: " << this->reusedNodeCoverage << std::endl;
-        outStream << "reusedMutNodes: " << this->reusedMutNodes << std::endl;
-        outStream << "reuseSizeBiggerThanHistMax: " << this->reuseSizeBiggerThanHistMax << std::endl;
-        outStream << "numWithSingletons: " << this->numWithSingletons << std::endl;
-        outStream << "maxSingletons: " << this->maxSingletons << std::endl;
-        outStream << "avgSingletons: " << (double)this->singletonSampleEdges / (double)this->numWithSingletons
+        outStream << "mutations: " << this->totalMutations.load() << std::endl;
+        outStream << "candidates: " << this->numCandidates.load() << std::endl;
+        outStream << "emptyMutations: " << this->emptyMutations.load() << std::endl;
+        outStream << "mutationsWithOneSample: " << this->mutationsWithOneSample.load() << std::endl;
+        outStream << "singletonSampleEdges: " << this->singletonSampleEdges.load() << std::endl;
+        outStream << "samplesProcessed: " << this->samplesProcessed.load() << std::endl;
+        outStream << "reusedNodes: " << this->reusedNodes.load() << std::endl;
+        outStream << "reusedExactly: " << this->reusedExactly.load() << std::endl;
+        outStream << "reusedNodeCoverage: " << this->reusedNodeCoverage.load() << std::endl;
+        outStream << "reusedMutNodes: " << this->reusedMutNodes.load() << std::endl;
+        outStream << "reuseSizeBiggerThanHistMax: " << this->reuseSizeBiggerThanHistMax.load() << std::endl;
+        outStream << "numWithSingletons: " << this->numWithSingletons.load() << std::endl;
+        outStream << "maxSingletons: " << this->maxSingletons.load() << std::endl;
+        outStream << "avgSingletons: " << (double)this->singletonSampleEdges.load() / (double)this->numWithSingletons.load()
                   << std::endl;
     }
 };
+
 
 class HaplotypeIndex;
 
